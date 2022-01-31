bf4ffad5f6055178432735ccfeee79cf1adf144a
skygit97 <sky_kumtong@yahoo.com>
2022-01-31 04:50:31 -0800

    Create login and signout methods

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,9 +1,22 @@
 class Api::SessionController < ApplicationController
+  # Login
   def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
 
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: ['The email and/or password combination is incorrect. Please try again.'], status: :unauthorized # 401
+    end
   end
 
+  #signout
   def destroy
-
+    logout!
+    render json: {}
   end
 end
