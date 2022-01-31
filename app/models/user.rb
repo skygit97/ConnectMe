620d6e212c05fe6adaf792db9b48dc63099d6859
skygit97 <sky_kumtong@yahoo.com>
2022-01-31 04:39:48 -0800

    create auth methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ApplicationRecord
 
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
-    user&.is_password?(password) ? user : mil
+    user&.is_password?(password) ? user : nil
   end
 
   private
