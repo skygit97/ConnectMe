4f05433778c873de43711e7587ac10eec931752c
skygit97 <sky_kumtong@yahoo.com>
2022-01-31 04:35:52 -0800

    Add session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
+    resources :session, only: [:create, :destroy]
     resources :users, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
