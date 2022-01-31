3f84d89c7bc88c2e4325a438ac56fa93c8a1652b
skygit97 <sky_kumtong@yahoo.com>
2022-01-31 02:21:45 -0800

    create users model

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  birth_date      :date             not null
+#  gender          :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+end
--- frontend/connect_me.jsx
+++ frontend/connect_me.jsx
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom';
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
 
-  ReactDOM.render(<h1>Welcome to ConnectME</h1>, root)
+  ReactDOM.render(<h1>Welcome to ConnectMe</h1>, root)
 })
